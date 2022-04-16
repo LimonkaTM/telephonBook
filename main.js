fda075d8a9f7b6ce9789f272333db60309415753
LimonkaTM <etuzin@mail.ru>
2022-04-16 19:04:55 +0300

    added new branch and modified files

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // Подключаем библиотеки и присваиваем их одноименным костантам
 const express = require('express'); // библиотека для веб-сервера
 const mysql2 = require('mysql2/promise'); // бибилотека для общения с БД
+const bodyParser = require('body-parser'); //библиотека для принятия данных из post запросов формы
 
 // создаём пул(подключение к базе данных)
 const pool = mysql2.createPool({
@@ -13,7 +14,10 @@ const pool = mysql2.createPool({
 // создаём наше прложение
 const app = express();
 
-// прослушиваем / для возврата hrml кода
+// подключаем к нашему приложению body-parser
+app.use(bodyParser.urlencoded({extended: true}));
+
+// прослушиваем обращение по главному пути для возврата hrml кода
 app.get('/', async function(req, res) {
   
   // выполняем запрос к БД
@@ -26,14 +30,67 @@ app.get('/', async function(req, res) {
   res.send(`<!DOCTYPE html>
     <html>
       <body>
-        <a href='/search'>Поиск</a>
+      <h1>Книга абонентов</h1>
+      <span>Список абонентов</span> <a href='/search'>Поиск</a>
+        <hr>
         <ul>
-          ${abonents.map(abonent => `<li><a href='/search/${abonent.id}'>${abonent.name}</a></li>`).join('')}
+          ${abonents.map(abonent => `<li><a href='/abonent-telephones/${abonent.id}'>${abonent.name}</a></li>`).join('')}
         </ul>
       </body>
     </html>
   `)
 })
+
+// страница с телефонными номерами конкретного абонента
+
+app.get('/abonent-telephones/:abonent_id', async function(req, res) {
+  const {abonent_id} = req.params;
+  const [telephones] = await pool.query(`SELECT * FROM telephones WHERE abonent_id = ?`, abonent_id);
+  const [[abonent]] = await pool.query(`SELECT * FROM abonents WHERE id = ?`, abonent_id)
+  res.send(`<!DOCTYPE html>
+    <html>
+      <body>
+        <h1>Телфонные номера абонента: ${abonent.name}</h1>
+        <a href='/'>Список абонентов</a> <a href='/search'>Поиск</a>
+        <hr>
+        <br>
+        ${`Найдено: ${telephones.length}`}
+        <ul>
+          ${telephones.map(telephones => `<li>${telephones.number} ${telephones.type} <a href="/remove-telephone/${telephones.id}">Удалить телефон</a></li>`).join('')}
+        </ul>
+        <form method='post' action='/add-telephone/${abonent.id}'>
+          <input type='text' name='number' placeholder='Номер телефона'/>
+          <input type='text' name='type' placeholder='Тип телефона'/>
+          <button type='submit'>Добавить</button>
+        </form>
+      </body>
+    </html>
+  `);
+})
+
+// обработка запроса на удаление номера
+
+app.get('/remove-telephone/:telephone_id', async function(req, res) {
+  const {telephone_id} = req.params;
+  const [[telephone]] = await pool.query(`SELECT * FROM telephones WHERE id = ?`, telephone_id);
+  await pool.query(`DELETE FROM telephones WHERE id = ?`, telephone_id)
+  res.redirect(`/abonent-telephones/${telephone.abonent_id}`)
+})
+
+// обработка запроса на добавление нового номера
+
+app.post('/add-telephone/:abonent_id', async function(req, res) {
+  const {abonent_id} = req.params;
+  const {number, type} = req.body;
+  await pool.query(`INSERT INTO telephones SET ?`, {
+    abonent_id,
+    number,
+    type
+  })
+  res.redirect(`/abonent-telephones/${abonent_id}`)
+})
+
+// страница поиска
 
 app.get('/search', async function(req, res) {
   const abonent_query = req.query.abonent_query;
@@ -44,36 +101,16 @@ app.get('/search', async function(req, res) {
   res.send(`<!DOCTYPE html>
     <html>
       <body>
-        <a href='/'>На главную</a>
-        <form method='get' action=''>
+        <h1>Поиск абонента</h1>
+        <a href='/'>Список абонентов</a><span> Поиск</span>
+        <hr>
+        <form method='get' action='/search'>
           <input type='text' name='abonent_query' placeholder='Введите запрос' value='${abonent_query ? abonent_query : ''}'/>
           <button type='submit'>Поиск</button>
         </form>
         ${`Найдено: ${telephones.length}`}
         <ul>
           ${telephones.map(telephones => `<li>${telephones.name} ${telephones.number}</li>`).join('')}
-        </ul>
-      </body>
-    </html>
-  `)
-})
-
-// моя попытка сделать открытие конкртеной страницы пользователя с номерами
-
-app.get('/search/:abonent_id', async function(req, res) {
-  const { abonent_id } = req.params;
-  const data = await pool.query(`SELECT abonents.name, telephones.number, telephones.type 
-  FROM abonents INNER JOIN telephones 
-  ON abonents.id = telephones.abonent_id 
-  WHERE abonents.id = ?`,`${abonent_id}`);
-  const abonent_telephones = data[0];
-  res.send(`<!DOCTYPE html>
-    <html>
-      <body>
-        <a href='/'>На главную</a>
-        ${`Найдено: ${abonent_telephones.length}`}
-        <ul>
-          ${abonent_telephones.map(abonent_telephones => `<li>${abonent_telephones.name} ${abonent_telephones.number}</li>`).join('')}
         </ul>
       </body>
     </html>
